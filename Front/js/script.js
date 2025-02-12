21b9b69481ed7b91a5d45f740b1148beb5b1f79c
Pedro Díaz Garduño <pedro.diaz@babelgroup.com>
2025-02-12 13:18:21 +0100

    Modificaciones en pestaña principal categoría

--- Front/js/script.js
+++ Front/js/script.js
@@ -1,3 +1,8 @@
-function abrirMenu(){
-    
+function abrirMenu(b){
+    var a = document.getElementById("menuDesplegable"+ b);
+    if(a.style.display == "block"){
+        a.style.display = "none";
+    }else{
+        a.style.display = "block";
+    }
 }
