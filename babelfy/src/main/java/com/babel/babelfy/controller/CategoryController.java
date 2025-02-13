1be84f7e19794872f7250b6c21bfed6fd29650d9
Raúl Llinares <raul.llinares.some@gmail.com>
2025-02-13 12:36:03 +0100

    Actualización del back en la rama

--- babelfy/src/main/java/com/babel/babelfy/controller/CategoryController.java
+++ babelfy/src/main/java/com/babel/babelfy/controller/CategoryController.java
@@ -1,4 +1,25 @@
 package com.babel.babelfy.controller;
 
+import com.babel.babelfy.model.Category;
+import com.babel.babelfy.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CategoryController {
+    @Autowired
+    private CategoryService service;
+
+
+    @PutMapping("")
+    public String putCategory(@RequestBody Category c){
+        if(service.modifyCategory(c)!=null){
+            return "Cambio realizado correctamente";
+        }else{
+            return "Cambio fallido" ;
+        }
+    }
+
 }
--- babelfy/src/main/java/com/babel/babelfy/service/CategoryService.java
+++ babelfy/src/main/java/com/babel/babelfy/service/CategoryService.java
@@ -1,4 +1,16 @@
 package com.babel.babelfy.service;
 
+import com.babel.babelfy.model.Category;
+import com.babel.babelfy.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repo;
+
+    public Category modifyCategory(Category c){
+            return repo.save(c);
+    }
+
 }
