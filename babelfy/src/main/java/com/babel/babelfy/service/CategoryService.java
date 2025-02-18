7fd374e85f59a8837faf1edbb23335b39d3cc52b
Pedro Díaz Garduño <pedro.diaz@babelgroup.com>
2025-02-18 10:33:21 +0100

    Modificar Categoria Merge conflicts solved

--- babelfy/src/main/java/com/babel/babelfy/service/CategoryService.java
+++ babelfy/src/main/java/com/babel/babelfy/service/CategoryService.java
@@ -49,4 +49,82 @@ public class CategoryService {
     }
 
 
+
+//    //ADD
+//    public CreateCategoryResponse create(CreateCategoryRequest newCat){
+//
+//        Category category = new Category(newCat.getName());
+//
+//        Category categorySaved = categoryRepository.save(category);
+//
+//        CreateCategoryResponse categoryResponse = new CreateCategoryResponse(
+//                categorySaved.getId(),
+//                categorySaved.getName(),
+//                categorySaved.getSongs().isEmpty() && categorySaved.getSongs().size() > 0
+//                        ? categorySaved.getSongs().stream().map(song ->SongDTO.SongToSongDTO(song)).toList()
+//                        : null);
+//
+//        return categoryResponse;
+//    }
+
+
+    //BUILDER
+
+
+//    public CreateCategoryResponse buildResponse (Category c){
+//        return CreateCategoryResponse.builder()
+//                .name(c.getName())
+//                .build()
+//                ;
+//    }
+
+    public CategoryDTO buildDTO(Category c) {
+        List<SongDTO> songs = c.getSongs() != null ?
+                c.getSongs().stream().map(this::convertToSongDTO).collect(Collectors.toList())
+                : new ArrayList<>();
+        return CategoryDTO.builder()
+                .id(c.getId())
+                .songs(songs)
+                .name(c.getName())
+                .build();
+
+    }
+
+
+    public Category buildCategory(CategoryDTO dto) {
+        List<Song> songs = dto.getSongs() != null ?
+                dto.getSongs().stream().map(this::convertToSong).collect(Collectors.toList())
+                : new ArrayList<>();
+
+        return Category.builder()
+                .songs(songs)
+                .name(dto.getName())
+                .build();
+    }
+    public SongDTO convertToSongDTO(Song song) {
+        return  SongDTO.builder()
+                .date(song.getDate())
+                .id_category(song.getCategory() != null ? song.getCategory().getId() : null)
+                .album(song.getAlbum())
+                .duration(song.getDuration())
+                .artist(song.getArtist())
+                .build();
+
+
+    }
+
+
+    private Song convertToSong(SongDTO songDTO) {
+        return Song.builder()
+                .name(songDTO.getName())
+                .date(songDTO.getDate())
+                .album(songDTO.getAlbum())
+                .artist(songDTO.getArtist())
+                .duration(songDTO.getDuration())
+
+                .build();
+    }
+
+
+
 }
