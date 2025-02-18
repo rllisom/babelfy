17cdaf693b15a899dd93744674da35039f612ead
Pedro Díaz Garduño <pedro.diaz@babelgroup.com>
2025-02-18 12:42:11 +0100

    ÚLtima actualización

--- babelfy/src/main/java/com/babel/babelfy/webconfig/WebConfig.java
+++ babelfy/src/main/java/com/babel/babelfy/webconfig/WebConfig.java
@@ -1,4 +1,4 @@
-package com.babel.babelfy.webconfig;
+package com.babel.babelfy.webConfig;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
